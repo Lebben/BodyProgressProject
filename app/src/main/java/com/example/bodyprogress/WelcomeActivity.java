cf729333862a4cf23887f19a192026a14d8db6a4
Lebben <leeviwlaaksonen@gmail.com>
2020-05-07 01:38:16 +0300

    5.5.2020 JavaDoc Done
    (cherry picked from commit e06a4845913d5519ae260ac76952a881bfb7dc27)

--- app/src/main/java/com/example/bodyprogress/WelcomeActivity.java
+++ app/src/main/java/com/example/bodyprogress/WelcomeActivity.java
@@ -23,6 +23,8 @@ public class WelcomeActivity extends AppCompatActivity {
          */
 
 
+
+
     }
     public void onButtonCreate (View v){
         Intent intent = new Intent(this, CreateProfiiliActivity.class);
